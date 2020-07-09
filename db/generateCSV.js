4c26a1d69c61f989016c78b0889ace907946f744
Chelsea Schmidt <cschmidt331@gmail.com>
2020-07-09 12:08:57 -0700

    Pseudocode data generation script for SQL CSV files

--- db/generateCSV.js
+++ db/generateCSV.js
@@ -1,18 +1,99 @@
 const fs = require('fs');
 const faker = require('faker');
+const _ = require('lodash');
 const { bedStrings, amenityStrings, titleStrings } = require('./seedStrings.js');
 
+// =====store re-used data between CSVs=====
+// store user ids
+// store listing ids
+// store amenity ids
+// store array of bedroom objects
+
 // =====generate amenities CSV (static)=====
-// print id | type | amenity headers
+// create write stream to amenities.csv in CSV files directory
+// store string starting with headers: id,type,amenity
 // for each amenity type...
   // for each amenity...
-    // print comma delimited: id | type | amenity
+    // generate and store unique id in amenity ids
+    // add to main string: id,type,amenity
+// write stored string to stream
+// on finish event
+  // log all data written
+  // close the stream
 
-// =====generate users CSV (50% of primary records)=====
-//
+  // -------> next dependent process
 
-// =====generate listings CSV (100% of primary records, referencing users)=====
+  // =====generate users CSV (50% of primary records)=====
+  // create write stream to users.csv in CSV files directory
+  // store string starting with headers: id,name,image
+  // for half of primary records target...
+    // generate and store unique id in user ids
+    // **try without a second unique id and check if breaks front-end**
+    // generate a name
+    // pick an image URL from among 1000 possible URLs (get 1000 onto S3)
+    // add to main string: id,name,image
+  // write stored string to stream
+  // on finish event
+    // log all data written
+    // close the stream
 
-// =====generate bedrooms CSV (2x - 10x primary records, referencing listings)=====
+    // -------> next dependent process
 
-// =====generate amenities for each listing CSV (5x - 20x primary records, referencing amenities and listings)=====
+    // =====generate listings CSV (100% of primary records, referencing users)=====
+    // create write stream to listings.csv in CSV files directory
+    // store string starting with headers: id | user_id | lId | ti | bo | gu | brs | beds | pB | privB
+    // for 100% of primary records target...
+      // generate and store unique id in listing ids
+      // get random user id from stored user ids
+      // generateTitle()
+      // generate body, five paragraphs with faker
+      // generate _random guests number between 1 and 10
+      // store number of bedrooms at 0
+      // store number of beds at 0
+      let hasCommonArea = false;
+      // for random number between 1 and 10...
+        // increment number of bedrooms
+        // create bedroom object with listingId property and all bed type properties : 0
+        // increment number of beds by random number between 1 and 5
+        // add unique bedroom id : random beds number generated property
+        // add property for bedroom name
+          // 1 in 3 chance of common area
+          // otherwise use bedroom + index
+        // push bedroom object to bedrooms array
+      const publicBaths = _.random(0, 10);
+      const privateBaths = _.random(publicBaths === 0 ? 1 : 0, 10);
+      // add to main string: data corresponding to headers
+    // write stored string to stream
+    // on finish event
+      // log all data written
+      // close the stream
+
+      // -------> next dependent process
+
+      // =====generate bedrooms CSV (2x - 10x primary records, referencing listings)=====
+      // create write stream to bedrooms.csv in CSV files directory
+      // store string starting with headers: id | listing_id | location | each type of bed
+      // for each bedroom in stored bedrooms...
+        // for number of beds in this bedroom...
+          // increment a randomly chosen bed type by 1
+        // add to main string: id | listing_id | location | each bed type number
+      // write stored string to stream
+      // on finish event
+        // log all data written
+        // close the stream
+
+        // -------> next dependent process
+
+        // =====generate amenities for each listing CSV (5x - 20x primary records)=====
+        // create write stream to amenities_listings.csv in CSV files directory
+        // store string starting with headers: id | amenity_id | listing_id | description
+        // for each listingId...
+          // store between 5 and 20 non-repeating amenityIds
+          // for each stored amenity...
+            // generate unique id
+            const description = _.random(0, 1) ? faker.lorem.sentences(1) : '';
+            // add to main string: id | amenityId | listingId | description
+        // write stored string to stream
+        // on finish event
+          // log all data written
+          // close the stream
