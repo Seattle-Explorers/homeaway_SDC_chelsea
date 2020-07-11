a564a47d90e7313ad80b5a494ea47ecf05f16ede
Chelsea Schmidt <cschmidt331@gmail.com>
2020-07-11 15:24:03 -0700

    Decouple room data seeding from listing data seeding to save memory

--- db/seed2.0/seed2.0.js
+++ db/seed2.0/seed2.0.js
@@ -1,30 +1,23 @@
 const fs = require('fs');
 const { Readable } = require('stream');
-const faker = require('faker');
 const path = require('path');
-const _ = require('lodash');
 const buildAmenities = require('./buildAmenities.js');
 const writeUsers = require('./writeUsers.js');
 const writeListings = require('./writeListings.js');
-const buildBedrooms = require('./buildBedrooms.js');
-const buildJunction = require('./buildJunction.js');
+const writeBedrooms = require('./writeBedrooms.js');
+const writeAmenitiesListings = require('./writeAmenitiesListings.js');
 
 const pathForGeneratedFiles = path.resolve(__dirname, '..', '..', '..', 'latitude_db_files');
 
-const targetedRecords = 100;
+const targetedRecords = 10000000;
 const imageBaseURL = 'https://fec-images-6-18-20.s3-us-west-2.amazonaws.com/latitude';
 const images = [];
 for (let i = 0; i < 1000; i += 1) {
   images.push(`${imageBaseURL}/img${i}.jpg`);
 }
 
-// =====store re-used data between CSVs=====
-let userIds;
-let listingIds;
-let amenityIds;
-let bedrooms;
-
 // =====generate amenities CSV=====
+let amenityIds;
 const amenitiesBlock = buildAmenities((ids) => { amenityIds = ids; });
 const readableA = Readable.from(amenitiesBlock);
 const writableA = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'amenities.csv'));
@@ -34,47 +27,24 @@ readableA.on('end', () => {
 
   // =====generate users CSV=====
   const writableU = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'users.csv'));
-  writeUsers(targetedRecords, images, writableU, (ids) => {
-    userIds = ids;
+  writeUsers(targetedRecords, images, writableU, (userIds) => {
     console.log('users data written to file');
     writableU.end();
 
-
-  // const readableU = Readable.from(usersBlock);
-  // const writeableU = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'users.csv'));
-  // readableU.pipe(writeableU);
-  // readableU.on('end', () => {
-  //   console.log('users data written to file');
-
     // =====generate listings CSV=====
     const writableL = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'listings.csv'));
-    writeListings(targetedRecords, userIds, writableL, (ids, rooms) => {
-      listingIds = ids;
-      bedrooms = rooms;
+    writeListings(targetedRecords, userIds, writableL, (listingIds) => {
       console.log('listings data written to file');
       writableL.end();
 
-    // console.log('made it here');
-    // const readableL = Readable.from(listingsBlock);
-    // const writableL = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'listings.csv'));
-    // readableL.pipe(writableL);
-    // readableL.on('end', () => {
-    //   console.log('listings data written to file');
-
       // =====generate bedrooms=====
-      const bedroomsBlock = buildBedrooms(bedrooms);
-      const readableB = Readable.from(bedroomsBlock);
       const writableB = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'bedrooms.csv'));
-      readableB.pipe(writableB);
-      readableB.on('end', () => {
+      writeBedrooms(listingIds, writableB, () => {
         console.log('bedrooms data written to file');
 
         // =====generate amenities for each listing=====
-        const amenityListingBlock = buildJunction(listingIds, amenityIds);
-        const readableAmLi = Readable.from(amenityListingBlock);
         const writableAmLi = fs.createWriteStream(path.resolve(pathForGeneratedFiles, 'amenities_listings.csv'));
-        readableAmLi.pipe(writableAmLi);
-        readableAmLi.on('end', () => {
+        writeAmenitiesListings(listingIds, amenityIds, writableAmLi, () => {
           console.log('amenities_listings data written to file');
         });
       });
---
+++ db/seed2.0/writeAmenitiesListings.js
@@ -0,0 +1,52 @@
+const _ = require('lodash');
+const faker = require('faker');
+const pickWeighted = require('../../util/pickWeighted.js');
+
+const writeAmenitiesListings = (listingIds, amenityIds, writable, logDone) => {
+  const amenityNumbers = [];
+  listingIds.forEach((listingId) => {
+    amenityNumbers.push(pickWeighted(_.range(5, 21), [5, 5, 5]));
+  });
+
+  let blocks = listingIds.length;
+  let i = 0;
+
+  const header = '"id","amenity_id","listing_id","description"\n';
+
+  writable.write(header, () => {
+    writeLines();
+
+    function writeLines() {
+      let okayToWrite = true;
+
+      while(blocks >= 0 && okayToWrite) {
+        const listingId = listingIds[i];
+        const shuffledAmenities = _.shuffle(amenityIds);
+        const listingAmenities = [];
+        const numberOfAmenities = amenityNumbers[i];
+        for (let j = 0; j < numberOfAmenities; j += 1) {
+          listingAmenities.push(shuffledAmenities[j]);
+        }
+        let block = '';
+        listingAmenities.forEach((amenityId, i) => {
+          const id = `am_li-${listingId}-${i}`;
+          const description = _.random(0, 1) ? faker.lorem.sentences(1) : '';
+          block += `"${id}","${amenityId}","${listingId}","${description}"\n`;
+        });
+        if (blocks === 0) {
+          writable.write(block, 'utf-8', logDone);
+          break;
+        } else {
+          okayToWrite = writable.write(block);
+          blocks -= 1;
+          i += 1;
+        }
+      }
+      if (blocks > 0) {
+        writable.once('drain', writeLines);
+      }
+    }
+  });
+};
+
+module.exports = writeAmenitiesListings;
