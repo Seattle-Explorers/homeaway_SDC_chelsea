701c85ff1cae2b620dd654678df1a9e00832736d
Chelsea Schmidt <cschmidt331@gmail.com>
2020-07-22 16:33:15 -0700

    Fix loader.io config

--- server/server.js
+++ server/server.js
@@ -8,15 +8,17 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
+
+app.get('/loaderio-e34f18fc91010c35600d737fd75801fa', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'loaderio-e34f18fc91010c35600d737fd75801fa.txt'));
+});
+
 app.use('/:id', express.static(path.join(__dirname, '../client/dist')));
+
 app.use(compression());
 
 app.get('/description/main.js/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/dist', 'main.js'));
-});
-
-app.get('/loaderio-e34f18fc91010c35600d737fd75801fa.txt', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'loaderio-e34f18fc91010c35600d737fd75801fa.txt'));
 });
 
 app.listen(port, () => {
