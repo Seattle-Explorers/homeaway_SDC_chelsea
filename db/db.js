9fe93b1e26121667e2ce8940e0b73bf92852be11
Chelsea Schmidt <cschmidt331@gmail.com>
2020-07-24 14:12:19 -0700

    Experiment with increasing client pool

--- db/db.js
+++ db/db.js
@@ -6,7 +6,7 @@ const password = process.env.PW || '';
 
 const pool = new Pool({
   host,
-  max: 15,
+  max: 30,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 20000,
   database: 'latitudedb',
