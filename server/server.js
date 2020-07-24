5b913cd054f96047b46e5755d5841dac599e0a1e
Chelsea Schmidt <cschmidt331@gmail.com>
2020-07-24 09:54:26 -0700

    Remove logging while testing performance

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ require('newrelic');
 const path = require('path');
 const express = require('express');
 const compression = require('compression');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const app = require('./app');
 
 const port = process.env.PORT || 3000;
 
-app.use(morgan('dev'));
+// app.use(morgan('dev'));
 
 app.get('/loaderio-e34f18fc91010c35600d737fd75801fa', (req, res) => {
   res.sendFile(path.resolve(__dirname, '..', 'loaderio-e34f18fc91010c35600d737fd75801fa.txt'));
